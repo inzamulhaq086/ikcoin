d49ad5a299ba11b30f2d5bd34c5fdc20248eb9aa
inzamulhaq086 <inzamulhaq086@gmail.com>
2022-01-18 23:33:00 +0600

    image with js,css link up

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IndexController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[IndexController::class, 'showIndex'])->name('home');
+Route::get('/admin',[DashboardController::class, 'index'])->name('dashboard');
